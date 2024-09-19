0072c095e934f75034b248da6032cf8df6c8c40f
Liverpool4ik <liverpool4ik@proton.me>
2024-09-19 10:23:58 +0300

    lesson-4__task-2

--- ___MyHomeWork/lesson__3/HW__task-2.js
+++ ___MyHomeWork/lesson__3/HW__task-2.js
@@ -1,16 +1,74 @@
-let day = new Date();
-let dayString = day.toLocaleDateString();
+let today = new Date();
+let todayTOString = today.toDateString();
+// let daysArray = [
+// 	"Понедельник",
+// 	"Вторник",
+// 	"Среда",
+// 	"Четверг",
+// 	"Пятница",
+// 	"Суббота",
+// 	"Воскресенье",
+// ];
+let day = today.getDay(1, 2);
 
-document.write(dayString);
-
-if (day !== true) {
-	// alert(
-	// 	"sorry, today is not a GOOD Day :)) because today is not the 16.th of September"
-	// );
-
+if (day === 2) {
 	document.write(
-		`<h1>"YYYESSS Today is september the 16.th  :)))) You'r the best):))</h1>`
+		`<h1>"YYYESSS Today is ${today}  :)))) You'r the best):))</h1>`
 	);
 } else {
-	document.write(`<h1>"nonono... todays is september the 16.th </h1>`);
+	document.write(`<h1>"You are not in TOday)))) </h1>`);
 }
+
+//=========================================//
+
+// let today2 = new Date();
+// let todayString = today2.toLocaleDateString();
+
+// let daysArray = [
+// 	"Понедельник",
+// 	"Вторник",
+// 	"Среда",
+// 	"Четверг",
+// 	"Пятница",
+// 	"Суббота",
+// 	"Воскресенье",
+// ];
+
+// function getDay() {
+// 	console.log(daysArray - 1);
+// }
+
+//=========================================//
+
+// let date = new Date();
+
+// let users = [
+// 	{
+// 		name: " Deim",
+// 		birthday: new Date(2011, 4, 13),
+// 	},
+// 	{
+// 		name: " Naim",
+// 		birthday: new Date(2010, 1, 12),
+// 	},
+// 	{
+// 		name: " Karim",
+// 		birthday: new Date(2017, 2, 13),
+// 	},
+// 	{
+// 		name: " Elisey",
+// 		birthday: new Date(2017, 4, 11),
+// 	},
+// 	{
+// 		name: " Fedor",
+// 		birthday: new Date(2017, 24, 12),
+// 	},
+// ];
+
+// users = users.filter(function (user) {
+// 	if (user.birthday.getFullYear() > 2015) return true;
+// });
+
+// console.log(users);
+
+//=========================================//
